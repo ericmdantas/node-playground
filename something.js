d7dab204d557da2963348788c0051809a1175466
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-01-10 23:30:45 -0200

    .

--- something.js
+++ something.js
@@ -2,16 +2,14 @@ export class Something {
   async doing() {
     console.log('a');
 
-    var x = await new Promise((res) => {
+    return await new Promise((res) => {
       console.log('b');
 
       res('c');
     });
-
-    console.log(x);
   }
 
   async yo() {
-    return await Promise.resolve(123456789);
+    return await this.doing();
   }
 }
