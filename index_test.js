4c7c3942fe6b59a41a9663e3579e0e86d4482568
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-04-01 10:25:53 -0300

    .

--- index_test.js
+++ index_test.js
@@ -1,5 +1,29 @@
 import test from 'ava';
 
-test('ava test - t', t => {
+test('ava test - 0', t => {
+  t.pass();
+});
+
+test('ava test - 1', t => {
+  t.pass();
+});
+
+test('ava test - 2', t => {
+  t.pass();
+});
+
+test('ava test - 3', t => {
+  t.pass();
+});
+
+test('ava test - 4', t => {
+  t.pass();
+});
+
+test('ava test - 5', t => {
+  t.pass();
+});
+
+test('ava test - 6', t => {
   t.pass();
 });
