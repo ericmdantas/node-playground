f844e32b6af43572635322583d28b487c07fab71
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-04-03 00:26:30 -0300

    .;.

--- index_test.js
+++ index_test.js
@@ -2,35 +2,22 @@ import test from 'ava';
 import {Something} from './';
 
 test('ava test - 0', t => {
+  let s = new Something();
+  s.doIt();
+
   t.pass();
 });
 
 test('ava test - 1', t => {
+  let s = new Something();
+  s.doIt();
+
   t.pass();
 });
 
 test('ava test - 2', t => {
-  t.pass();
-});
-
-test('ava test - 3', t => {
-  t.pass();
-});
-
-test('ava test - 4', t => {
-  t.pass();
-});
-
-test('ava test - 5', t => {
-  t.pass();
-});
-
-test('ava test - 6', t => {
-  t.pass();
-});
-
-test('ava test - 7', t => {
   let s = new Something();
-
+  s.doIt();
+  
   t.pass();
 });
